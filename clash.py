b501fa3d6a52344f5a9673b5d733c5cb935284df
Oleg <oiprokhorov@gmail.com>
2020-10-18 23:34:32 +0300

    trying to add new Warrior classes

--- clash.py
+++ clash.py
@@ -1,14 +1,13 @@
 from warrior import Warrior
 
 
-class Clash(Warrior):
+class Clash:
     def __init__(self, player1, player2):
-        super().__init__(self)
         self.player1 = player1
         self.player2 = player2
         self.wins = 0
 
-    def start(self):
+    def start_clash(self):
         self.player1 = Warrior(self.player1.name)
         self.player2 = Warrior(self.player2.name)
         print(f"You have faced an enemy: {self.player2.name} ({self.player2.hp} HP) \n\
@@ -17,13 +16,13 @@ What are you going to do: fight (f) or run away (r)?")
             try:
                 your_choice = input()
                 if your_choice == 'f':
-                    print("\u2694 Clash! \u2694")
+                    print("\u2694 CLASH! \u2694")
                     return self.combat()
                 elif your_choice == 'r':
                     print("You ran away from the enemy. \n\
 You're still alive, but you haven't gained any glory.")
                     print()
-                    return Clash.start(self)
+                    return Clash.start_clash(self)
                 else:
                     print("Make your decision: \u2694 (f) or \U0001F3C3 (r)?")
             finally:
@@ -40,9 +39,9 @@ You're still alive, but you haven't gained any glory.")
                 self.wins += 1
                 print(
                     f"\U0001F339 Congratulations, brave {self.player1.name}! "
-                    f"You've overwhelmed the {self.player2.name} \U0001F339")
+                    f"You've overcame the {self.player2.name} \U0001F339")
                 print()
-                Clash.start(self)
+                Clash.start_clash(self)
             else:
                 print(f"\n\
 \U0001F480 You've been killed. \n\
@@ -54,10 +53,19 @@ You're still alive, but you haven't gained any glory.")
                         restart = input()
                         if restart == 'y':
                             self.wins = 0
-                            Clash.start(self)
+                            Clash.start_clash(self)
                         elif restart == 'n':
                             break
                         else:
-                            print("Choose yes (y) or no (n)?")
+                            print("Choose: yes (y) or no (n)?")
                     finally:
                         pass
+
+'''
+if __name__ == '__main__':
+    player1 = Warrior('Knight')
+    player2 = Warrior('Ogre')
+
+    play = Clash(player1, player2)
+    play.start_clash()
+'''
--- cs_game.py
+++ cs_game.py
@@ -1,10 +1,17 @@
 from clash import Clash
 from warrior import Warrior
+from warrior import Ogre
+from warrior import Goblin
+from warrior import Dragon
+import random
 
 
-class Game(Clash, Warrior):
-    def __init__(self, player1, player2):
-        super().__init__(player1, player2)
+class Game:
+    def __init__(self, player1_id, player2_id):
+        self.player1 = player1
+        self.player2 = player2
+        self.player1_id = player1_id
+        self.player2_id = player2_id
 
     def intro(self):
         print("the  C O C K Y   S T R O L L E R \n\
@@ -12,12 +19,59 @@ class Game(Clash, Warrior):
 Here is a small intro. \n\
 You are a daring vagabond. You wade through the woods coming into sudden clashes and covering your name "
               "with glory or shame.\n")
-        Clash.start(self)
+        # player1_id = 0
+        # player2_id = 0
+        # self.choose_your_destiny()
+        # self.select_player1(player1_id)
+        # self.select_player2(player2_id)
+        start_game = Clash(self.player1, self.player2)
+        return start_game.start_clash()
+
+    def choose_your_destiny(self):
+        print("Select your character: \n"
+              "1 - Knight \n"
+              "2 - Ogre \n"
+              "3 - Goblin \n"
+              "4 - Dragon (you bloody cheater!)")
+        while True:
+            try:
+                player_id = int(input())
+                print(type(player_id), player_id)
+                if player_id in range(1, 5):
+                    print(f"You chose the {self.player1.name}'s way.")
+                    # self.player1 = Warrior(self.player1.player_id)        # Херня какая-то
+                    return player_id
+                else:
+                    print("Choose your destiny (1 to 4):")
+            except ValueError:
+                print("Choose your destiny (1 to 4):")
+            finally:
+                pass
+'''
+    def select_player1(self, player1_id):       # сделать в цикле; относить к классу (или экземпляру?) по id
+        if player1_id == 1:
+            self.player1 = Warrior(self.player1.name)
+        elif player1_id == 2:
+            self.player1 = Ogre(self.player1.name)
+        elif player1_id == 3:
+            self.player1 = Goblin(self.player1.name)
+        elif player1_id == 4:
+            self.player1 = Dragon(self.player1.name)
+        return self.player1
+'''
+
+"""
+    def select_player2(self, player2_id):
+        player2_id = random.randint(1, 4)
+        for i in range(1, 5):
+            self.player2 = Warrior(player2_id)      # привязать выбор класса к player2_id
+        return self.player2
+"""
 
 
 if __name__ == '__main__':
-    player1 = Warrior('Knight')
-    player2 = Warrior('Ogre')
+    player1 = Warrior()
+    player2 = Warrior()
 
     play = Game(player1, player2)
     play.intro()
